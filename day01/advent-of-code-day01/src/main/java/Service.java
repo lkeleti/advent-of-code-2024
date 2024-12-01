f0e7b31f13f8d7ae1c652f55aeee6f2e93fa7524
lkeleti <keleti.kaszlo@vipmail.hu>
2024-12-01 09:44:02 +0100

    Solved both parts

--- day01/advent-of-code-day01/src/main/java/Service.java
+++ day01/advent-of-code-day01/src/main/java/Service.java
@@ -2,21 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 public class Service {
+    public final List<Integer> left = new ArrayList<>();
+    public final List<Integer> right = new ArrayList<>();
+
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
             while ((line = br.readLine()) != null) {
+                String[] datas = line.split(" {3}");
+                left.add(Integer.parseInt(datas[0]));
+                right.add(Integer.parseInt(datas[1]));
             }
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read file: " + path, ioe);
         }
     }
     public long partOne() {
-        return 0;
+        List<Integer> leftSorted = left.stream().sorted().toList();
+        List<Integer> rightSorted = right.stream().sorted().toList();
+        int summa = 0;
+        for (int i =0; i< leftSorted.size(); i++) {
+            summa += Math.abs(rightSorted.get(i) - leftSorted.get(i));
+        }
+        return summa;
     }
     public int partTwo() {
-        return 0;
+        Map<Integer, Integer> rightFreq = new TreeMap<>();
+        for (int defRight: right) {
+            if (rightFreq.containsKey(defRight)) {
+                rightFreq.put(defRight, rightFreq.get(defRight) + 1 );
+            } else{
+                rightFreq.put(defRight, 1);
+            }
+        }
+        return getSimilarityScore(rightFreq);
+    }
+
+    private int getSimilarityScore(Map<Integer, Integer> rightFreq) {
+        int summa = 0;
+        for (int defLeft : left) {
+            if (rightFreq.containsKey(defLeft)) {
+                summa += (rightFreq.get(defLeft) * defLeft);
+            }
+        }
+        return summa;
     }
 }
