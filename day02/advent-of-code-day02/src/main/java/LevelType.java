44e878b239e49b4293a4a2ce670dd048db0d9e9d
lkeleti <keleti.kaszlo@vipmail.hu>
2024-12-02 06:32:16 +0100

    Day02 part1 completed

---
+++ day02/advent-of-code-day02/src/main/java/LevelType.java
@@ -0,0 +1,3 @@
+public enum LevelType {
+    INCREASING, DECREASING
+}
---
+++ day02/advent-of-code-day02/src/main/java/Service.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+    public final List<List<Integer>> reports = new ArrayList<>();
+    public void readInput(Path path) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] datas = line.split(" ");
+                List<Integer> numbers = new ArrayList<>();
+                for (String data : datas) {
+                    numbers.add(Integer.parseInt(data));
+                }
+                reports.add(numbers);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file: " + path, ioe);
+        }
+    }
+    public long partOne() {
+        int counter = 0;
+        for (List<Integer> numbers : reports) {
+            LevelType defType = checkLevelType(numbers.get(0), numbers.get(1));
+            boolean safe = true;
+            for (int i = 1; i <numbers.size();i++) {
+                if (!(checkLevelType(numbers.get(i-1), numbers.get(i)) == defType &&
+                        checkDistance(numbers.get(i-1), numbers.get(i)))) {
+                    safe = false;
+                }
+            }
+            if (safe) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private boolean checkDistance(Integer num1, Integer num2) {
+        if (Math.abs(num1 - num2) > 0 && Math.abs(num1 - num2) < 4) {
+            return true;
+        }
+        return false;
+    }
+
+    private LevelType checkLevelType(int num1, int num2) {
+        return  num1 > num2? LevelType.INCREASING:LevelType.DECREASING;
+    }
+
+    public int partTwo() {
+        return 0;
+    }
+}
