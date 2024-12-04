f0a2352d915cb636dd24ff1945829825d8f9c9b6
lkeleti <keleti.kaszlo@vipmail.hu>
2024-12-04 21:06:55 +0100

    Day04 both parts completed

--- day04/advent-of-code-day04/src/main/java/Service.java
+++ day04/advent-of-code-day04/src/main/java/Service.java
@@ -26,6 +26,7 @@ public class Service {
             throw new IllegalStateException("Cannot read file: " + path, ioe);
         }
     }
+
     public long partOne() {
         int counter = 0;
         for (int j = 0; j < colMax; j++) {
@@ -43,24 +44,14 @@ public class Service {
         for (int row = i; row < Math.min(rowMax, i + word.length()); row++) {
             defWord += board.get(row).get(j);
         }
-        if (defWord.equals(word)) {
-            counter++;
-        }
-        if (backward(defWord).equals(word)) {
-            counter++;
-        }
+        counter += checkWords(word, defWord);
 
         //right
         defWord = "";
         for (int col = j; col < Math.min(colMax, j + word.length()); col++) {
             defWord += board.get(i).get(col);
         }
-        if (defWord.equals(word)) {
-            counter++;
-        }
-        if (backward(defWord).equals(word)) {
-            counter++;
-        }
+        counter += checkWords(word, defWord);
 
         //diag down-right
         defWord = "";
@@ -69,12 +60,7 @@ public class Service {
                 defWord += board.get(i+k).get(j+k);
             }
         }
-        if (defWord.equals(word)) {
-            counter++;
-        }
-        if (backward(defWord).equals(word)) {
-            counter++;
-        }
+        counter += checkWords(word, defWord);
 
         //diag up
         defWord = "";
@@ -83,13 +69,18 @@ public class Service {
                 defWord += board.get(i+k).get(j-k);
             }
         }
+        counter += checkWords(word, defWord);
+        return counter;
+    }
+
+    private int checkWords(String word, String defWord) {
         if (defWord.equals(word)) {
-            counter++;
+            return 1;
         }
         if (backward(defWord).equals(word)) {
-            counter++;
+            return 1;
         }
-        return counter;
+        return 0;
     }
 
     private String backward(String wordToBackward) {
@@ -102,6 +93,30 @@ public class Service {
     }
 
     public int partTwo() {
+        int counter = 0;
+        for (int j = 0; j < colMax-2; j++) {
+            for (int i = 0; i < rowMax-2; i++) {
+                counter += findWord2("MAS", i, j);
+            }
+        }
+        return counter;
+    }
+
+    private int findWord2(String word, int i, int j) {
+        String word1 = "";
+        String word2 = "";
+
+        word1 += board.get(i).get(j);
+        word1 += board.get(i+1).get(j+1);
+        word1 += board.get(i+2).get(j+2);
+
+        word2 += board.get(i+2).get(j);
+        word2 += board.get(i+1).get(j+1);
+        word2 += board.get(i).get(j+2);
+
+        if ((checkWords(word,word1) != 0) && (checkWords(word,word2) != 0)) {
+            return 1;
+        }
         return 0;
     }
 }
