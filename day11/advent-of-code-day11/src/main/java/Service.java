484b336e1f777c534e1b05562f11a4f6e9438a51
lkeleti <keleti.laszlo@vipmail.hu>
2024-12-11 16:19:40 +0100

    Day11 updated

--- day11/advent-of-code-day11/src/main/java/Service.java
+++ day11/advent-of-code-day11/src/main/java/Service.java
@@ -2,21 +2,56 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Service {
+    public List<Long> stones = new ArrayList<>();
+
     public void readInput(Path path) {
+
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
             while ((line = br.readLine()) != null) {
+                String[] datas = line.split(" ");
+                for (String data: datas) {
+                    stones.add(Long.parseLong(data));
+                }
             }
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read file: " + path, ioe);
         }
     }
     public long partOne() {
-        return 0;
+        for (int i = 0; i < 25; i++) {
+            calcChanges();
+            System.out.println(stones.size());
+        }
+        return stones.size();
     }
-    public int partTwo() {
-        return 0;
+
+    private void calcChanges() {
+        List<Long> newStones = new ArrayList<>();
+        for (long stone: stones) {
+            String stoneString = String.valueOf(stone);
+            if (stone == 0) {
+                newStones.add(1L);
+            } else if (stoneString.length() % 2 == 0) {
+                int middle = stoneString.length() /2;
+                newStones.add(Long.parseLong(stoneString.substring(0,middle)));
+                newStones.add(Long.parseLong(stoneString.substring(middle)));
+            } else {
+                newStones.add(stone*2024);
+            }
+        }
+        stones = newStones;
+    }
+
+    public long partTwo() {
+        for (int i = 0; i < 50; i++) {
+            calcChanges();
+            System.out.println(stones.size());
+        }
+        return stones.size();
     }
 }
