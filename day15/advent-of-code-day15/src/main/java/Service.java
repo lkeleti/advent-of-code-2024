08a771020a85fd57b42a812de928377406c6d5d2
lkeleti <keleti.kaszlo@vipmail.hu>
2024-12-16 07:37:42 +0100

    Day15 updated

--- day15/advent-of-code-day15/src/main/java/Service.java
+++ day15/advent-of-code-day15/src/main/java/Service.java
@@ -8,8 +8,11 @@ import java.util.List;
 public class Service {
 
     private final List<List<Character>> board = new ArrayList<>();
+    private final List<List<Character>> board2 = new ArrayList<>();
     private int maxX;
     private int maxY;
+    private int max2X;
+    private int max2Y;
     private List<Character> directions = new ArrayList<>();
     private final Cord up = new Cord(0,-1);
     private final Cord right = new Cord(1,0);
@@ -47,11 +50,37 @@ public class Service {
         maxY = board.size();
     }
     public long partOne() {
-        Cord startPos = findRobot();
+        createBoard2();
+        Cord startPos = findRobot(1);
         for (char direction : directions) {
             startPos = moveRobot(startPos, direction);
         }
         return calcGPS();
+    }
+
+    private void createBoard2() {
+        for (int y = 0; y < maxY; y++) {
+            List<Character> row = new ArrayList<>();
+            for (int x= 0; x < maxX; x++) {
+                char defValue = board.get(y).get(x);
+                if ( defValue == '#') {
+                    row.add('#');
+                    row.add('#');
+                } else if (defValue == 'O') {
+                    row.add('[');
+                    row.add(']');
+                } else if (defValue =='.') {
+                    row.add('.');
+                    row.add('.');
+                } else if (defValue == '@') {
+                    row.add('@');
+                    row.add('.');
+                }
+            }
+            board2.add(row);
+        }
+        max2X = board2.getFirst().size();
+        max2Y = board2.size();
     }
 
     private long calcGPS() {
@@ -68,6 +97,12 @@ public class Service {
 
     private void drawBoard() {
         for (List<Character> row: board) {
+            System.out.println(row);
+        }
+        System.out.println("");
+    }
+    private void drawBoard2() {
+        for (List<Character> row: board2) {
             System.out.println(row);
         }
         System.out.println("");
@@ -124,10 +159,26 @@ public class Service {
         return defDirection;
     }
 
-    private Cord findRobot() {
-        for (int y = 0; y < maxY; y++) {
-            for (int x= 0; x < maxX; x++) {
-                if (board.get(y).get(x) == '@') {
+    private Cord findRobot(int part) {
+        int maxXValue;
+        int maxYValue;
+
+        if (part == 1) {
+            maxXValue = maxX;
+            maxYValue = maxY;
+        } else {
+            maxXValue = max2X;
+            maxYValue = max2Y;
+        }
+        for (int y = 0; y < maxYValue; y++) {
+            for (int x= 0; x < maxXValue; x++) {
+                char defValue;
+                if (part == 1) {
+                    defValue = board.get(y).get(x);
+                } else {
+                    defValue = board2.get(y).get(x);
+                }
+                if ( defValue== '@') {
                     return new Cord(x, y);
                 }
             }
@@ -135,8 +186,50 @@ public class Service {
         return new Cord(-1, -1);
     }
 
-    public int partTwo() {
-        return 0;
+    public long partTwo() {
+        Cord startPos = findRobot(2);
+        drawBoard2();
+        for (char direction : directions) {
+            startPos = moveRobot2(startPos, direction);
+            drawBoard2();
+        }
+        return calcGPS();
+    }
+
+    private Cord moveRobot2(Cord startPos, char direction) {
+        Cord defDirection = getDirection(direction);
+        Cord nextStep = new Cord(startPos.getPosX() + defDirection.getPosX(), startPos.getPosY() + defDirection.getPosY());
+        char nextValue = board2.get(nextStep.getPosY()).get(nextStep.getPosX());
+        if (nextValue == '.') {
+            board2.get(nextStep.getPosY()).set(nextStep.getPosX(),'@');
+            board2.get(startPos.getPosY()).set(startPos.getPosX(),'.');
+            return nextStep;
+        } else if (nextValue == '#') {
+            return startPos;
+        } else {
+            List<Cord> boxes = new ArrayList<>();
+            boxes.add(nextStep);
+            while (!(nextValue == '.' || nextValue == '#')) {
+                nextStep = new Cord(nextStep.getPosX() + defDirection.getPosX(), nextStep.getPosY() + defDirection.getPosY());
+                nextValue = board2.get(nextStep.getPosY()).get(nextStep.getPosX());
+                if (nextValue == '[' || nextValue == ']') {
+                    boxes.add(nextStep);
+                } else if (nextValue == '.') {
+                    board2.get(nextStep.getPosY()).set(nextStep.getPosX(),']');
+                    for (int i = boxes.size() - 1; i > 0; i--) {
+                        if (i % 2 == 0) {
+                            board2.get(boxes.get(i).getPosY()).set(boxes.get(i).getPosX(), '[');
+                        } else {
+                            board2.get(boxes.get(i).getPosY()).set(boxes.get(i).getPosX(), ']');
+                        }
+                    }
+                    board2.get(boxes.getFirst().getPosY()).set(boxes.getFirst().getPosX(),'@');
+                    board2.get(startPos.getPosY()).set(startPos.getPosX(),'.');
+                    return boxes.getFirst();
+                }
+            }
+            return startPos;
+        }
     }
 }
 
