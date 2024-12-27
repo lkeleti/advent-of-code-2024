44d2e117e52bfd586b33731e3e3dfc8cad00c2b6
lkeleti <keleti.kaszlo@vipmail.hu>
2024-12-27 22:31:16 +0100

    Day17 both parts completed

--- day17/advent-of-code-day17/src/main/java/Service.java
+++ day17/advent-of-code-day17/src/main/java/Service.java
@@ -3,23 +3,24 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Service {
-    private int registerA;
-    private int registerB;
-    private int registerC;
+    private long registerA;
+    private long registerB;
+    private long registerC;
 
     private final List<Integer> program = new ArrayList<>();
     public void readInput(Path path) {
         try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
             while ((line = br.readLine()) != null) {
-                registerA = Integer.parseInt(line.replace("Register A: ", ""));
+                registerA = Long.parseLong(line.replace("Register A: ", ""));
                 line = br.readLine();
-                registerB = Integer.parseInt(line.replace("Register B: ", ""));
+                registerB = Long.parseLong(line.replace("Register B: ", ""));
                 line = br.readLine();
-                registerC = Integer.parseInt(line.replace("Register C: ", ""));
+                registerC = Long.parseLong(line.replace("Register C: ", ""));
                 line = br.readLine();
                 line = br.readLine().replace("Program: ","");
                 String[] datas = line.split(",");
@@ -32,101 +33,139 @@ public class Service {
         }
     }
     public String partOne() {
-        List<Integer> output = new ArrayList<>();
-        for (int ip = 0; ip < program.size(); ip+=2) {
-            int opcode =  program.get(ip);
-            int operand = program.get(ip + 1);
-            operand = getOperandValue(operand);
-            switch (opcode) {
-                case 0 : adv(operand);
-                break;
-                case 1 : bxl(operand);
-                    break;
-                case 2 : bst(operand);
-                    break;
-                case 3 : ip = jnz(operand, ip);
-                    break;
-                case 4 : bxc();
-                    break;
-                case 5 : out(operand, output);
-                    break;
-                case 6 : bdv(operand);
-                    break;
-                case 7 : cdv(operand);
-                    break;
-            }
-        }
+        List<Integer> output = compute(registerA);
         return String.join(",", output.stream().map(String::valueOf).toList());
     }
 
-    private void cdv(int operand) {
-        int result =  (int)(registerA / Math.pow(2,operand));
+    private List<Integer> compute(Long regA) {
+        registerA = regA;
+        List<Integer> output = new ArrayList<>();
+        for (int ip = 0; ip < program.size(); ip+=2) {
+            int opcode = program.get(ip);
+            int operand = program.get(ip + 1);
+
+            switch (opcode) {
+                case 0:
+                    adv(combo(operand));
+                    break;
+                case 1:
+                    bxl(combo(operand));
+                    break;
+                case 2:
+                    bst(combo(operand));
+                    break;
+                case 3:
+                    ip = jnz(combo(operand), ip);
+                    break;
+                case 4:
+                    bxc();
+                    break;
+                case 5:
+                    output.add(out(combo(operand)));
+                    break;
+                case 6:
+                    bdv(combo(operand));
+                    break;
+                case 7:
+                    cdv(combo(operand));
+                    break;
+            }
+        }
+        return output;
+    }
+
+    private void cdv(long operand) {
+        Long result = (registerA / (int)Math.pow(2,operand));
         registerC = result;
     }
 
-    private void bdv(int operand) {
-        int result =  (int)(registerA / Math.pow(2,operand));
+    private void bdv(long operand) {
+        Long result = (registerA / (int)Math.pow(2,operand));
         registerB = result;
     }
 
-    private void out(int operand, List<Integer> output) {
-        output.add(operand % 8);
+    private int out(long operand) {
+        return (int)(operand % 8);
     }
 
     private void bxc() {
         registerB = registerB ^ registerC;
     }
 
-    private int jnz(int operand, int ip) {
+    private int jnz(long operand, int ip) {
         if (registerA != 0) {
-            return operand - 2;
+            return (int)operand - 2;
         }
         return ip;
     }
 
-    private void bst(int operand) {
-        int result = operand % 8;
-        String binaryResult = String.format("%16s", Integer.toBinaryString(result)).replace(' ', '0');
+    private void bst(long operand) {
+        long result = operand % 8;
+        String binaryResult = String.format("%16s", Long.toBinaryString(result)).replace(' ', '0');
         binaryResult = binaryResult.substring(binaryResult.length()-3);
-        registerB = Integer.parseInt(binaryResult,2);
+        registerB = Long.parseLong(binaryResult,2);
     }
 
-    private void bxl(int operand) {
+    private void bxl(long operand) {
         registerB = registerB ^ operand;
     }
 
-    private void adv(int operand) {
-        registerA = (int)(registerA / Math.pow(2,operand));
+    private void adv(long operand) {
+        registerA = (registerA / (int)Math.pow(2,operand));
     }
 
-    private int getOperandValue(int operand) {
+    private long combo(int operand) {
         switch (operand) {
             case 4:
-                operand = registerA;
-                break;
+                return registerA;
             case 5:
-                operand = registerB;
-                break;
+                return registerB;
             case 6:
-                operand = registerC;
-                break;
+                return registerC;
             case 7:
                 throw new IllegalArgumentException("Invalid operand number 7");
         }
         return operand;
     }
 
-    public int partTwo() {
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            program.clear();
-            readInput(Path.of("src/main/resources/input.txt"));
-            registerA = i;
-            String newValue = partOne();
-            String programValue = String.join(",", program.stream().map(String::valueOf).toList());
-            if (newValue.equals(programValue)) {
-                return i;
+    public long partTwo() {
+        List<Long> candadates = new ArrayList<>();
+        candadates.add(0L);
+
+        for (int j = program.size()-1; j >= 0; j--) {
+            List<Long> nextCandidates = new ArrayList<>();
+
+            for (Long val: candadates) {
+                for (int i = 0; i < 8; i++) {
+                    Long target = (val << 3) + i;
+                    List<Integer> output = compute(target);
+                    List<Integer> sublist = program.subList(j, program.size());
+                    if (output.equals(sublist)) {
+                        nextCandidates.add(target);
+                    }
+                }
             }
+            candadates.clear();
+            candadates.addAll(nextCandidates);
         }
-        return -1;
+        return candadates.getFirst();
     }
 }
+/*
+1 - 0-7
+2 - 8-63
+3 - 64-511
+4 - 512-4095
+5 - 4096-32767
+6 - 32768-262143
+7 - 262144-2097151
+8 - 2097152-16777215
+9 - 16777216-134217727
+10- 134217728-1073741823
+11- 1073741824-8589934591
+12- 8589934592-68719476735
+13- 68719476736-549755813887
+14- 549755813888-4398046511103
+15- 4398046511104-35184372088831
+16- 35184372088832-281474976710655
+*/
